7cd562437d5766b6a581343a4b38d3ad2aec4da9
Gia Duc <giaduc19042004@gmail.com>
2025-03-28 20:59:04 +0700

    no important

--- src/pages/Boards/_id.jsx
+++ src/pages/Boards/_id.jsx
@@ -4,26 +4,26 @@ import BoardBar from './BoardBar/BoardBar';
 import BoardContent from './BoardContent/BoardContent';
 import { useEffect, useState } from 'react';
 
-import { mockData } from '~/apis/mock-data';
+// import { mockData } from '~/apis/mock-data';
 import { fetchBoardDetailsAPI } from '~/apis';
 
 function Board() {
-  // const [board, setBoard] = useState(null)
+  const [board, setBoard] = useState(null)
 
-  // useEffect(() => {
-  //   // Tạm thời fix cứng boardId, flow chuẩn chỉnh về sau khi học nâng cao là chúng ta sẽ sử dụng react-router-dom để lấy chuẩn boardId từ URL về
-  //   const boardId = '67e40670fc439a357ec01d02'
-  //   // callAPI
-  //   fetchBoardDetailsAPI(boardId).then(board => {
-  //     setBoard(board)
-  //   })
-  // }, [])
+  useEffect(() => {
+    // Tạm thời fix cứng boardId, flow chuẩn chỉnh về sau khi học nâng cao là chúng ta sẽ sử dụng react-router-dom để lấy chuẩn boardId từ URL về
+    const boardId = '67e40670fc439a357ec01d02'
+    // callAPI
+    fetchBoardDetailsAPI(boardId).then(board => {
+      setBoard(board)
+    })
+  }, [])
 
   return (
     <Container disableGutters maxWidth={false} sx={{ height: '100vh' }}>
       <AppBar />
-      <BoardBar board={mockData.board} />
-      <BoardContent board={mockData.board} />
+      <BoardBar board={board} />
+      <BoardContent board={board} />
     </Container>
   )
 }
